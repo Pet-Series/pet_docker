62a747036e2ea58aae613024d3301159b94c523e
Stefan Kull <stefan.kull@gmail.com>
2023-06-08 22:39:09 +0200

    Added GUI support (RViz & Gazebo)

--- pet-mk-iv-simulation/docker_run.sh
+++ pet-mk-iv-simulation/docker_run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Run docker image from https://hub.docker.com/repository/docker/kullken/ros1_amd64
+
+# Connect to, an running, container using shell/bash by: 
+# $ docker exec -it pet-mk-iv-simulation_container bash
 docker run \
  -it \
  --rm \
  --network=host \
  --privileged \
+ --volume="/tmp/.X11-unix:/tmp/.X11-unix" \
+ --env DISPLAY=${DISPLAY} \
+ --name pet-mk-iv-simulation_container \
  kullken/ros1_amd64:pet-mk-iv-simulation
