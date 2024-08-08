e8c9dc88a8d18d260dafcdb38759a6478eaa0c85
SeniorKullken <stefan.kull@gmail.com>
2024-08-08 18:26:38 +0200

    PC/AMD64 Docker source files - ROS2 Jazzy LTS - Ubuntu 24.04 LTS Noble

---
+++ amd64-jazzy-full/docker_rm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Remove existing container (Prereq: Status=Exited/Stoped)
+# More info: https://hub.docker.com/repositories/kullken
+ docker container rm jazzy-full-container
---
+++ amd64-jazzy-full/docker_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run docker image from https://hub.docker.com/repositories/kullken
+ docker run \
+  -it \
+  --name jazzy-full-container \
+  --env DISPLAY=$DISPLAY \
+  --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+  --volume="/etc/localtime:/etc/localtime:ro" \
+  --network=host \
+ kullken/ros2_amd64:jazzy-full
