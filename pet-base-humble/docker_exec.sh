a2b9c9ce582aff0e2b5668b51cde706429c260ea
SeniorKullken <stefan.kull@gmail.com>
2024-10-05 12:32:27 +0200

    Updated Focal(20.04) => Jazzy(22.04) for Humble

--- pet-base-humble/docker_exec.sh
+++ pet-base-humble/docker_exec.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-# Build Dockerfile in current directory = '/home/sk/ws_ros2/src/pet_docker/pet-mk-viii-runtime-humble'
+# Start bash in existing container (Status Up/Paused).
 #----------------------
 # Maintainer: stefan.kull@gmail.com
 # Licens: MIT
 # https://github.com/Pet-Series
 #
-# "humble"= ROS2
-# "focal"  = Ubuntu 20.04 
+# "humble" = ROS2
+# "jammy"  = Ubuntu 22.04 
 #
 # More info: 
 #    - https://github.com/Pet-Series/pet_docker
 #    - https://hub.docker.com/repositories/kullken
 #    - https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
 #    - https://hub.docker.com/r/arm64v8/ros/tags?page=1&name=humble
-docker build --tag kullken/ros2_arm64v8:pet-mk-viii-runtime .
+#
+docker exec -it --env HOSTUSER=$USER pet-base-humble_container bash
--- pet-humble/docker_run_humble.sh
+++ pet-humble/docker_run_humble.sh
@@ -5,15 +5,15 @@
 # https://hub.docker.com/r/arm64v8/ros/tags
 # https://hub.docker.com/r/arm64v8/ros/tags?name=jazzy
 docker run -it \
- --rm \
- --network=host \
- --privileged \
- --user=$(id -u $USER):$(id -g $USER) \
- --workdir="/home/$USER" \
- --volume="/home/$USER:/home/$USER" \
- --volume="/etc/group:/etc/group:ro" \
- --volume="/etc/passwd:/etc/passwd:ro" \
- --volume="/etc/shadow:/etc/shadow:ro" \
- --volume="/etc/sudoers:/etc/sudoers:ro" \
- --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
-arm64v8/ros:humble-ros-base-jammy
+    --rm \
+    --network=host \
+    --privileged \
+    --user=$(id -u $USER):$(id -g $USER) \
+    --workdir="/home/$USER" \
+    --volume="/home/$USER:/home/$USER" \
+    --volume="/etc/group:/etc/group:ro" \
+    --volume="/etc/passwd:/etc/passwd:ro" \
+    --volume="/etc/shadow:/etc/shadow:ro" \
+    --volume="/etc/sudoers:/etc/sudoers:ro" \
+    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    arm64v8/ros:humble-ros-base-jammy
