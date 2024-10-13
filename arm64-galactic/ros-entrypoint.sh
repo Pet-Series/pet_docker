c2e4ce81f324406fca42bb9650fa7ab007e8a1fb
SeniorKullken <stefan.kull@gmail.com>
2024-10-13 16:48:35 +0200

    Cleaning up folders...

--- arm64-galactic/ros-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Launch from Docker image
-# 
-set -e
-source /opt/ros/noetic/setup.bash
-source /opt/ros/galactic/setup.bash
-#source /home/pet/ws_ros2-galactic/install/setup.bash
-exec "$@"
