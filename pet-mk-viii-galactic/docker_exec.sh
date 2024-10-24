6c499a54e0a4c31c8423261c530d38f1bba82164
SeniorKullken <stefan.kull@gmail.com>
2024-10-24 22:12:48 +0200

    More Jazzy stuff updated. Overlay-layer for testing

--- pet-mk-viii-galactic/docker_exec.sh
+++ pet-mk-viii-galactic/docker_exec.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-# Push up a local Docker Image to DockerHub repository.
-#
-# Prerequisite:
-#   $ docker login -u "kullken" -p "@@@@@@@@" docker.io
-#
+# Start bash in existing container (Status Up/Paused).
 #----------------------
 # Maintainer: stefan.kull@gmail.com
 # Licens: MIT
 # https://github.com/Pet-Series
-#
+
 # "galactic" = ROS2
 # "focal"  = Ubuntu 20.04 
-#
+
 # More info: 
 #    - https://github.com/Pet-Series/pet_docker
 #    - https://hub.docker.com/repositories/kullken
 #    - https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
 #    - https://hub.docker.com/r/arm64v8/ros/tags?page=1&name=galactic
-docker push kullken/ros2_arm64v8:pet-mk-viii-galactic
+docker exec -it --env HOSTUSER=$USER pet-base-galactic_container  bash
