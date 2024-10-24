66f2a8db221b8163f60644c8e45972fce3a01a2f
SeniorKullken <stefan.kull@gmail.com>
2024-10-24 22:11:35 +0200

    Humble-stuff. Next try after testing iteration...

--- arm64-pet-humble/docker_exec.sh
+++ arm64-pet-humble/docker_exec.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 # Start bash in existing container (Status Up/Paused).
+#
+# Prerequisite - On docker host: 
+#   I) Raspberry Pi(arm64v8) with Ubuntu & Docker installed.
+#  II) Docker Container with name 'pet-base-humble_container' is Up or Paused.
 #----------------------
 # Maintainer: stefan.kull@gmail.com
-# Licens: MIT
+# License: MIT
 # https://github.com/Pet-Series
 #
-# "humble" = ROS2
-# "jammy"  = Ubuntu 22.04 
+#  - ROS2 version: ROS2/humble(arm64v8)
+#  - OS version:   Ubuntu 22.04/jammy(arm64v8)
 #
 # More info: 
-#    - https://github.com/Pet-Series/pet_docker
-#    - https://hub.docker.com/repositories/kullken
-#    - https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
-#    - https://hub.docker.com/r/arm64v8/ros/tags?page=1&name=humble
-#
+#  - https://github.com/Pet-Series/pet_docker
+#  - https://hub.docker.com/repositories/kullken
+#  - https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
+#  - https://hub.docker.com/r/arm64v8/ros/tags?page=1&name=humble
 docker exec -it --env HOSTUSER=$USER pet-base-humble_container bash
--- arm64-pet-humble/docker_run.sh
+++ arm64-pet-humble/docker_run.sh
@@ -1,8 +1,39 @@
 #!/bin/bash
 # Run docker image from https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
+# Entrypoint CMD = /bin/bash
+#
+# Prerequisite - On docker host: 
+#       I)  That '$ xhost local:docker' is started before the container is started. To enable GUI/X11 applications inside the container
+#       II) Docker host must clone https://github.com/Pet-Series/Pet-Mk-VIII.git => /home/pet/ws_ros2/src
+#----------------------
+# Maintainer: stefan.kull@gmail.com
+# License: MIT
+# https://github.com/Pet-Series
+#
+#  - ROS2 version: ROS2/humble(arm64v8)
+#  - OS version:   Ubuntu 22.04/jammy(arm64v8)
+#
+# More info: 
+#  - https://github.com/Pet-Series/pet_docker
+#  - https://hub.docker.com/repositories/kullken
+#  - https://hub.docker.com/repository/docker/kullken/ros2_arm64v8
+#  - https://hub.docker.com/r/arm64v8/ros/tags?page=1&name=humble
+
 docker run \
-    -it \
+    --interactive \
+    --tty \
     --rm \
-    --network=host \
+    --network host \
     --privileged \
-    kullken/ros2_arm64v8:pet-base-humble
+    --volume /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    --volume /etc/localtime:/etc/localtime:ro \
+    --volume /proc/device-tree:/proc/device-tree:ro \
+    --env DISPLAY=${DISPLAY} \
+    --env HOSTUSER=$USER \
+    --device /dev/gpiomem:rw \
+    --device /dev/i2c-1:rw \
+    --device /dev/spidev0.0:rw \
+    --device /dev/spidev0.1:rw \
+    --device /dev/vchiq:r \
+    --name pet-base-humble_container \
+    kullken/ros2_arm64v8:pet-base-humble /bin/bash
